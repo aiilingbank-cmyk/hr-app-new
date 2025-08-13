157d4ae7a9e1b69b107ac40cfeeb00e748010486
aiilingbank-cmyk <aiilingbank@gmail.com>
2025-08-13 11:57:02 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './', // relative base works for any repo name
+  // ใส่ path ของ GitHub Pages ให้ตรงกับชื่อรีโป
+  base: '/hr-app-new/',
+  plugins: [react()],
   server: { port: 5173, host: true }
 })
